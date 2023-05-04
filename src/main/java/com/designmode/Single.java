bbc9bcff9c8129057232059ffa72dea08f8ee422
yanghao33 <yanghao33@58.com>
2023-05-04 23:19:40 +0800

    master

---
+++ src/main/java/com/designmode/Single.java
@@ -0,0 +1,15 @@
+package com.designmode;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 单例
+ */
+public class Single {
+
+
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock(true);
+    }
+
+}
